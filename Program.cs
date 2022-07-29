81fcc11107a312584fb54883d2d4919df0885ee2
Azhar Kurmangaliyeva <azharkas15@gmail.com>
2022-07-29 14:20:50 +0600

    Second task solved

--- Program.cs
+++ Program.cs
@@ -10,12 +10,35 @@ namespace SecondSeminar
             // 456 -> 5
             // 782 -> 8
             // 918 -> 1
-
+                /*
                 Console.WriteLine("Программа выводит вторую цифру трехзначного цисла");
                 Console.Write("Введи трёхзначное число ");
+
                 int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
                 string stringNumber = Convert.ToString(threeDigitNumber);
-                Console.WriteLine("вторая цифра этого числа: "+stringNumber[1]);
+                Console.WriteLine("Вторая цифра этого числа: "+stringNumber[1]);
+                */
+
+
+            // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+            // 645 -> 5
+            // 78 -> третьей цифры нет
+            // 32679 -> 6
+
+                Console.WriteLine("Программа выводит третью цифру трехзначного цисла");
+                Console.Write("Введи число ");
+
+                int anyNumber = Convert.ToInt32(Console.ReadLine());
+                string anyNumberText = Convert.ToString(anyNumber);
+
+                if (anyNumberText.Length > 2){
+                Console.WriteLine("Третья цифра: " + anyNumberText[2]);
+                }
+                else {
+                Console.WriteLine("Это не трехзначное число");
+                }
+
+
         }
     }
 }
