4db2d771f177f9e48c433c2772c98c7a11ece119
Azhar Kurmangaliyeva <azharkas15@gmail.com>
2022-07-29 14:24:39 +0600

    Third task solved

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ namespace SecondSeminar
             // 645 -> 5
             // 78 -> третьей цифры нет
             // 32679 -> 6
-
+                /*
                 Console.WriteLine("Программа выводит третью цифру трехзначного цисла");
                 Console.Write("Введи число ");
 
@@ -37,6 +37,32 @@ namespace SecondSeminar
                 else {
                 Console.WriteLine("Это не трехзначное число");
                 }
+                */
+
+            //Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+            // 6 -> да
+            // 7 -> да
+            // 1 -> нет
+
+                Console.WriteLine("Программа, которая проверяет дни недели");
+                Console.Write("Введите цифру, обозначающую день недели ");
+
+                int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+                void CheckingTheDayOfTheWeek (int dayNumber) {
+
+                if (dayNumber == 6 || dayNumber == 7) {
+                Console.WriteLine("Это выходной день");
+                }
+
+                else if (dayNumber < 1 || dayNumber > 7) {
+                    Console.WriteLine("Это не день недели!");
+                }
+
+                else Console.WriteLine("Это рабочий день");
+                }
+
+                CheckingTheDayOfTheWeek(dayNumber);
 
 
         }
