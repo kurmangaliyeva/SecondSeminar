76f71f8bf5b76ca2650683a31fa3a7903c71fe6c
Azhar Kurmangaliyeva <azharkas15@gmail.com>
2022-07-29 14:17:17 +0600

    First task solved

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SecondSeminar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+            // 456 -> 5
+            // 782 -> 8
+            // 918 -> 1
+
+                Console.WriteLine("Программа выводит вторую цифру трехзначного цисла");
+                Console.Write("Введи трёхзначное число ");
+                int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+                string stringNumber = Convert.ToString(threeDigitNumber);
+                Console.WriteLine("вторая цифра этого числа: "+stringNumber[1]);
+        }
+    }
+}
